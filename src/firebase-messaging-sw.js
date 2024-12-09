3cb5e3654caacf6a8a18fb64d40a2b8adbc75790
Fernando <ferna.mace.34@gmail.com>
2024-12-09 11:14:50 -0400

    Initial Commit

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -6,12 +6,12 @@ importScripts(
 );
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsAX684t2dnAw7omIy8Q65nMfu4Ec8Uwo",
-  authDomain: "test-only-1cba0.firebaseapp.com",
-  projectId: "test-only-1cba0",
-  storageBucket: "test-only-1cba0.firebasestorage.app",
-  messagingSenderId: "19780782070",
-  appId: "1:19780782070:web:8abe4f6459cfb8ea33e9d7",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
